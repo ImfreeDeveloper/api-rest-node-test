6b0fa7053b77a209ec17f2cb37e4f45f5e25a7c1
Артур Галеев <a.galeev@tapir.ws>
2020-08-21 13:27:02 +0500

    connect BD

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors') // cors запросы если другой домен
+const morgan = require('morgan') // логирование
 // роуты
 const authRoutes = require('./routes/auth')
+const analyticsRoutes = require('./routes/analytics')
+const categoryRoutes = require('./routes/category')
+const orderRoutes = require('./routes/order')
+const positionRoutes = require('./routes/position')
+
+const keys = require('./config/keys')
 
 const app = express()
 
+// Подключение к БД mongo
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected.');
+  })
+  .catch(err => console.log(err))
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
 app.use('/api/auth', authRoutes)
+app.use('/api/analytics', analyticsRoutes)
+app.use('/api/category', categoryRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/position', positionRoutes)
 
 module.exports = app
