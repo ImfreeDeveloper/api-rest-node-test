f530f1798ac5a47cbc63050ba195015749bc3b45
Артур Галеев <a.galeev@tapir.ws>
2020-09-02 00:40:44 +0500

    react form

--- client/src/pages/authentication/index.js
+++ client/src/pages/authentication/index.js
@@ -20,25 +20,14 @@ const Authentication = props => {
                 }}
                 validate={values => {
                   const errors = {}
-                  if (!values.username) {
-                    errors.username = 'Заполни username блэээээт'
-                  }
                   if (!values.email) {
-                    errors.email = 'Заполни email блэээээт';
+                    errors.email = 'email обязательный';
                     //eslint-disable-next-line
                   } else if (!values.email.match(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/)) {
-                    errors.email = "Нормально делай email да"
+                    errors.email = "некорректный email"
                   }
                   if (!values.password) {
-                    errors.password = 'Заполни password блэээээт'
-                  }
-                  if (!values.confirmPassword) {
-                    errors.confirmPassword = 'Заполни confirmPassword блэээээт'
-                  } else if (values.confirmPassword !== values.password) {
-                    errors.confirmPassword = 'Не совпадалово'
-                  }
-                  if (!values.acceptTerms) {
-                    errors.acceptTerms = 'Соглайшайся епрст'
+                    errors.password = 'Пароль обязательный'
                   }
                   return errors
                 }}
@@ -47,21 +36,6 @@ const Authentication = props => {
                   <>
                     <div className="card-content">
                       <p className="mb-5 pb-2 is-size-3 has-text-weight-light">{pageTitle}</p>
-                      <Field name="username">
-                        {({ input, meta }) => (
-                          <div className="field">
-                            <p className="control">
-                              <input
-                                {...input}
-                                type="text"
-                                className={`input${(meta.error && meta.touched ? ' is-danger' : '')}`}
-                                placeholder="Имя"
-                              />
-                              {meta.error && meta.touched && <span className="help is-danger">{meta.error}</span>}
-                            </p>
-                          </div>
-                        )}
-                      </Field>
                       <Field name="email">
                         {({ input, meta }) => (
                           <div className="field">
@@ -87,36 +61,6 @@ const Authentication = props => {
                                 className={`input${(meta.error && meta.touched ? ' is-danger' : '')}`}
                                 placeholder="Пароль"
                               />
-                              {meta.error && meta.touched && <span className="help is-danger">{meta.error}</span>}
-                            </p>
-                          </div>
-                        )}
-                      </Field>
-                      <Field name="confirmPassword">
-                        {({ input, meta }) => (
-                          <div className="field">
-                            <p className="control">
-                              <input
-                                {...input}
-                                type="password"
-                                className={`input${(meta.error && meta.touched ? ' is-danger' : '')}`}
-                                placeholder="Подтверждение пароля"
-                              />
-                              {meta.error && meta.touched && <span className="help is-danger">{meta.error}</span>}
-                            </p>
-                          </div>
-                        )}
-                      </Field>
-                      <Field name="acceptTerms" type="checkbox">
-                        {({ input, meta }) => (
-                          <div className="field">
-                            <p className="control">
-                              <label className="checkbox">
-                                <input
-                                  {...input}
-                                  className={meta.error && meta.touched ? 'is-danger' : ''}
-                                />&nbsp;Согласен чё
-                              </label>
                               {meta.error && meta.touched && <span className="help is-danger">{meta.error}</span>}
                             </p>
                           </div>
